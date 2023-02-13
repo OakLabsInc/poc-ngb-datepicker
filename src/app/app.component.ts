ec5cb4d5e738d0d8a6923629ccceeb04a17a06f5
Richard Joffray <Richard.Joffray@verifone.com>
2023-02-13 09:50:53 -0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,67 @@
 import { Component } from '@angular/core';
+import { NgbDate, NgbCalendar, NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+
 })
 export class AppComponent {
   title = 'poc-ngb-datepicker';
+  isActive: boolean = true;
+  hoveredDate: NgbDate | null = null;
+
+	fromDate: NgbDate = new NgbDate(1789, 7, 14) ;
+	toDate: NgbDate | null | undefined
+
+	constructor(calendar: NgbCalendar) {
+		this.fromDate = calendar.getToday();
+		this.toDate = calendar.getNext(calendar.getToday(), 'd', 10);
+	}
+
+	onDateSelection(date: NgbDate) {
+		if (!this.fromDate && !this.toDate) {
+			this.fromDate = date;
+		} else if (this.fromDate && !this.toDate && date.after(this.fromDate)) {
+			this.toDate = date;
+		} else {
+			this.toDate = null;
+			this.fromDate = date;
+		}
+	}
+
+	isHovered(date: NgbDate) {
+		return (
+			this.fromDate && !this.toDate && this.hoveredDate && date.after(this.fromDate) && date.before(this.hoveredDate)
+		);
+	}
+
+	isInside(date: NgbDate) {
+		return this.toDate && date.after(this.fromDate) && date.before(this.toDate);
+	}
+
+	isRange(date: NgbDate) {
+		return (
+			date.equals(this.fromDate) ||
+			(this.toDate && date.equals(this.toDate)) ||
+			this.isInside(date) ||
+			this.isHovered(date)
+		);
+	}
+  toggleMenu(){
+    
+    this.isActive = !this.isActive;
+  }
+
+  getToDate(myDate:any) {
+    
+    if (!myDate) {
+      return " "
+    } 
+    return myDate?.month + '/' + myDate?.day + '/' + myDate?.year
+    
+  }
 }
